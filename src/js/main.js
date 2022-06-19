48e3b5adf0ed65fc387ce4f6704a5b69d9a8dc99
sarathcr <sarathcr9177725@gmail.com>
2022-06-19 19:57:11 +0530

    Updated completed UI for Home and layout pages

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import '../scss/main.scss';
 
 // Load Bootstrap init
 import {initBootstrap} from "./bootstrap";
+import { initForms } from './forms';
+import { initSidebar } from './sidebar';
 
 // Loading bootstrap with optional features
 initBootstrap({
@@ -10,6 +12,11 @@ initBootstrap({
   popover: true,
   toasts: true,
 });
+
+// sidebar functionality
+initSidebar()
+
+initForms()
 
 // Your app code
 console.log(`Hello ${process.env.HELLO}`);
